38a3a17ab4875ccdee249628e959fb920562066e
Shahan K <shahan.k.code@gmail.com>
2024-12-07 12:46:19 -0500

    initial ember prover service

---
+++ examples/ember/service/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/brevis-network/brevis-sdk/examples/ember"
+	"github.com/brevis-network/brevis-sdk/sdk/prover"
+)
+
+func main() {
+	proverService, err := prover.NewService(
+		&ember.AppCircuit{},
+		prover.ServiceConfig{
+			SetupDir: "$HOME/circuitOut",
+			SrsDir:   "$HOME/kzgsrs",
+			RpcURL:   "http://localhost:8547",
+			ChainId:  412346,
+		})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	proverService.Serve("localhost", 33247)
+}
